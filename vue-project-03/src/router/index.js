51f9c1b7af4b0050d5f9f3069f88bfffae9433de
Arcueid <451676498@qq.com>
2022-05-23 17:10:10 +0800

    login

--- vue-project-03/src/router/index.js
+++ vue-project-03/src/router/index.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import LoginView from "../views/LoginView.vue";
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path:'/home',
+    component:()=> import('../views/HomeView.vue')
+  },
+  {
+    path: '/login',
+    component:LoginView
+  }
+]
 
 const router = new VueRouter({
   mode: 'history',
